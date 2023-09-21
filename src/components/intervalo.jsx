e77f948f773b4822921fe65110411d452ff172d2
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-21 11:39:32 -0300

    update: added redux reducers

--- src/components/intervalo.jsx
+++ src/components/intervalo.jsx
@@ -1,19 +1,27 @@
 import Card from './Card'
 import './intervalo.css'
 import React from 'react'
+
+import { useSelector, useDispatch } from 'react-redux'
+import { updateMin, updateMax } from '../store/actions/numberSlice'
+
 export default function Intervalo(props) {
-    const min = props.min;
-    const max = props.max;
+
+    const min = useSelector((state) => state.number.min)
+    const max = useSelector((state) => state.number.max)
+
+    const dispatch = useDispatch()
+
     return (
         <Card title="Intervalo de números" red>
             <div className='Intervalo'>
                 <span>
-                    <strong>Mínimo</strong>
-                    <input type="number" value={min}  onChange={(e)=> props.onMinChanged(+e.target.value)}/>
+                    <strong>Mínimo:</strong>
+                    <input type="number" value={min} onChange={(e)=> dispatch(updateMin(e.target.value))} />
                 </span>
                 <span>
-                    <strong>Máximo</strong>
-                    <input type="number" value={max}  onChange={(e)=> props.onMaxChanged(+e.target.value)} />
+                    <strong>Máximo:</strong>
+                    <input type="number" value={max} onChange={(e) => dispatch(updateMax(e.target.value))} />
                 </span>
             </div>
         </Card>
